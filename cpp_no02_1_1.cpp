aa3ea9e9020c77c91fdeae4e86aa7a28b95a1e6c
taez329 <xowjd0329@naver.com>
2024-01-01 09:06:31 +0000

    참조자 활용 함수 작성

---
+++ cpp_no02_1_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+/*int ref_plus(int &ref1)
+{
+	ref1++;
+	return ref1;
+}
+증가, 부호변환의 기능만 해주면 됨, 값을 원하는 함수가 아님 return 안 시켜도됨
+*/
+
+void ref_plus(int &ref1)
+{
+	ref1++;
+}
+
+void ref_changesign(int &ref2)
+{
+	ref2 *= -1;
+}
+
+int main()
+{
+	int num1 = 10; 
+	int num2 = 5;
+	
+	//num1 = ref_plus(num1); 
+	//num2 = ref_changesign(num2); 이미 함수를 통과하면서 값이 바뀌므로 저장할 필요 x
+	ref_plus(num1);
+	ref_changesign(num2);
+	cout << "num1: "<< num1 << endl;
+	cout << "num2: "<< num2 << endl;
+	return 0;
+
+}
