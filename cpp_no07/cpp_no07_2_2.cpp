ae618f3d8ccb1391149f38c7a278bf52823e53e1
taez329 <ptj1541@gmail.com>
2024-01-21 05:47:38 +0000

    추가부분이 있을때 기존부분은 상위클래스의 생성자, 함수 이용하기

--- cpp_no07/cpp_no07_2_2.cpp
+++ cpp_no07/cpp_no07_2_2.cpp
@@ -14,9 +14,9 @@ public:
 	//멤버변수는 this->title 이런식으로 표시 멤버변수는 title 이런 식으로 표시
 		: price(__price)
 	{
-		title=new char(strlen(__title)+1);
+		title=new char[strlen(__title)+1];
 		strcpy(title, __title);
-		isbn=new char(strlen(__isbn)+1);
+		isbn=new char[strlen(__isbn)+1];
 		strcpy(isbn, __isbn);
 		//this->title=new char(strlen(title)+1); 이런 식으로 this 포인터 이용 가능
 	}
@@ -39,15 +39,15 @@ private:
 	char * DRMKey;
 public:
 	EBook(char* _title, char* _isbn, int _price, char* _DRMKey)
-		:Book(_title, _isbn, _price)
+		:Book(_title, _isbn, _price) //상위 클래스의 생성자를 호출해서 멤버변수 초기화
 	{
-		DRMKey=new char(strlen(_DRMKey)+1);
+		DRMKey=new char[strlen(_DRMKey)+1];
 		strcpy(DRMKey, _DRMKey);
 	}
 	
 	void ShowEBookInfo()
 	{
-		ShowBookInfo();
+		ShowBookInfo(); //상위 클래스에 있는 함수 쓰기
 		cout<<"인증키: "<<DRMKey<<endl;
 	}
 	~EBook()
